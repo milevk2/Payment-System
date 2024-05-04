5c95156068ef192bda1a082781db825a80b03556
Mitko Milev <milevk2@gmail.com>
2024-05-04 23:29:50 +0300

    server configurations are now in a separate file

--- src/server/configurations/serverConfig.js
+++ src/server/configurations/serverConfig.js
@@ -1,11 +1,16 @@
 const { urlencoded } = require('body-parser');
 const serverLogger = require('../middlewares/serverLogger.js');
+const authMiddleWare = require('../middlewares/authMiddleware.js');
 const handlebarsConfig = require('./handlebarsConfig.js');
+const cookieParser = require('cookie-parser');
+
 
 function serverConfig(app) {
 
     app.use(urlencoded({ extended: true }));
+    app.use(cookieParser());
     app.use(serverLogger);
+    app.use(authMiddleWare);
     handlebarsConfig(app);
 }
 
