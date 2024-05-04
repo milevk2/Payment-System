2dbdeaf3fbbae92af283035367e78cb40cdc7406
Mitko Milev <milevk2@gmail.com>
2024-05-04 23:41:18 +0300

    added an endpoint for card deletion service. Small improvements on other endpoints. Deleted old endpoints

--- src/server/server.js
+++ src/server/server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const dbConnect = require('./configurations/dbConfig.js');
 const serverConfig = require('./configurations/serverConfig.js');
 const userService = require('./services/userService.js');
+const cardService = require('./services/cardService.js');
 
 const app = express();
 serverConfig(app);
 
-try {
-    dbConnect();
-    console.log('Successfully connected to the DB!');
-}
-catch (err) {
+(async function () {
+    try {
+        await dbConnect();
+        console.log('Successfully connected to the DB!');
+    }
+    catch (err) {
 
-    console.log(err);
-}
+        console.log(err);
+    }
+})()
 
 app.get('/', (req, res) => {
 
@@ -33,22 +36,66 @@ app.post('/login', async (req, res) => {
 
     try {
 
-       const jwtToken =  await userService.login(email, password);
-       res.cookie('jwtToken', jwtToken, { httpOnly: true, maxAge: 3600000 })
-       res.send('Login successful!');
-
+        const jwtToken = await userService.login(email, password);
+        res.cookie('jwtToken', jwtToken, { httpOnly: true, maxAge: 3600000 })
+        res.redirect('/dashboard');
     }
     catch (err) {
         console.log(err);
         res.send('User name or password do not match!');
     }
+})
+
+app.get('/dashboard', async (req, res) => {
+
+    if (!req.userData) {
+
+        res.send('You are not authorized!')
+        return;
+    }
+    const cards = await cardService.getUserCards(req.userData.userId)
+
+    res.render('cards', { cards })
+
+})
+
+app.post('/createCard', async (req, res) => {
+
+    const { schemaProvider } = req.body;
+
+    if (!req.userData) {
+
+        res.send('You are not authorized!')
+        return;
+    }
+
+    const { userId, first_name, last_name } = req.userData;
+
+    try {
+
+        await userService.addCard(userId, schemaProvider, `${first_name} ${last_name}`);
+        res.redirect('/dashboard');
+    }
+    catch (err) {
+
+        res.render('error', { err })
+        console.log(err.message);
+    }
+})
+
+app.get('/deleteCard/:id', async (req, res) => {
+
+    const cardId = req.params.id;
+    const { userId } = req.userData;
+
+    await cardService.deleteCard(userId, cardId);
+    res.redirect('/dashboard');
 
 })
 
 app.get('/register', (req, res) => {
 
     res.render('register');
-
 })
 
 app.post('/register', async (req, res) => {
@@ -65,39 +112,5 @@ app.post('/register', async (req, res) => {
 })
 
 
-app.get('/getMerchant', async (req, res) => {
-
-    try {
-        const merchantDetails = await getMerchantDetails(req.query.MID);
-
-        if (merchantDetails.length == 0) throw new Error('The resource was not found!');
-        res.render('merchantProfile.hbs', { ...merchantDetails });
-    }
-    catch (err) {
-
-        res.status(404).render('404.hbs');
-
-    }
-})
-
-app.post('/addComment', async (req, res) => {
-
-    const { MID, comment } = req.body;
-
-    try {
-        const merchantDetails = await addComment(MID, comment);
-
-        console.log(merchantDetails);
-        res.render('merchantProfile.hbs', { ...merchantDetails })
-    }
-    catch (err) {
-
-        console.log(err);
-        res.render('404.hbs')
-
-    }
-
-
-})
 
 app.listen(3000, () => console.log('The server is listening on port 3000!'))
