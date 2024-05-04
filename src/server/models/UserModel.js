9d6f7b8d0276c709ad2018a7515a5cdfdbb0d48a
Mitko Milev <milevk2@gmail.com>
2024-05-04 23:34:00 +0300

    now User model has cards properties which is an array of CardSchema. It has a validation function cardArrayLimit. UserSchema.pre now returns if _vv is undefined (because this function should only work on document creation, not on document modification)

--- src/server/models/UserModel.js
+++ src/server/models/UserModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const { CardSchema } = require('./CardModel.js');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
 
     first_name: {
         type: String,
@@ -18,14 +19,25 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-      },
+    },
+    
     address: String,
     phone: String,
     DOB: Date,
+
+    cards: {
+        type: [CardSchema],
+        validate: [cardArrayLimit, '{PATH} exceeds the limit of 5']
+    },    
     balance: Number
 })
 
-userSchema.virtual("rePassword").set(function (value) {
+function cardArrayLimit(arr) {
+
+    return arr.length <=5 ;
+}
+
+UserSchema.virtual("rePassword").set(function (value) {
 
     if (value != this.password) {
         throw new mongoose.MongooseError("Password missmatch!");
@@ -33,13 +45,15 @@ userSchema.virtual("rePassword").set(function (value) {
 });
 
 
-userSchema.pre("save", async function () {
+UserSchema.pre("save", async function () {
 
-    const hash = await bcrypt.hash(String(this.password), 10);
+    if (this.__v !==  undefined) return;  //if we do not do this, each modification of the document will trigger this function, which is not expected behaviour;
+
+    const hash = await bcrypt.hash(this.password, 10);  
     this.password = hash;
     this.balance = 0;
 
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
