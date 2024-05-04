21d9dd325dc872d2ebe6e265a1eeabc5e5889b70
Mitko Milev <milevk2@gmail.com>
2024-05-04 23:37:08 +0300

    added an addCard service, which creates a card document and then attaches it to user.cards array (User Model)

--- src/server/services/userService.js
+++ src/server/services/userService.js
@@ -1,10 +1,14 @@
 const User = require('../models/UserModel.js');
 const bcrypt = require('bcrypt');
 const jwtSecret = require('../keys/jwtSecret.js')
-const jwt = require('jsonwebtoken');
+const jwt = require('../lib/promisifiedJwt.js')
+const { createCard } = require('./cardService.js');
+const { MongooseError } = require('mongoose');
+
+
 
 exports.createUser = async (data) => {
-    
+
     return await User.create(data);
 }
 
@@ -15,22 +19,33 @@ exports.autenticateUser = () => {
 
 exports.login = async (email, password) => {
 
-  
+
     const user = await User.findOne({ email });
 
     if (!user) {
-        throw new Error('User not found!')
+        throw new MongooseError('User not found!');
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-       throw new Error('Invalid password');
+        throw new MongooseError('Invalid password');
     }
 
-    const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: '1h' });
+    const token = await jwt.sign({ userId: user._id, first_name: user.first_name, last_name: user.last_name }, jwtSecret, { expiresIn: '1h' });
 
-    console.log(token);
     return token;
+}
 
+exports.addCard = async (userId, schemaProvider, cardholder) => {
+    
+    const user = await User.findOne({ _id: userId });
+
+    if (user.cards.length >= 5) {
+        throw new MongooseError('The card limit of 5 has been reached!');
+    }
+
+    const card = await createCard(userId, schemaProvider, cardholder);
+    user.cards.push(card);
+    await user.save();
 }
