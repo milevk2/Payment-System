3a3c468fa846c8b5f7762d57e9a52f61b0445f8e
Mitko Milev <milevk2@gmail.com>
2024-05-05 14:27:31 +0300

    added a maskCard mongoose middleware, card number is now a string, balance removed from model

--- src/server/models/CardModel.js
+++ src/server/models/CardModel.js
@@ -1,14 +1,25 @@
 const mongoose = require('mongoose');
 
+
 const CardSchema = new mongoose.Schema({
 
     owner_id: String,
     cardholder: String,
-    card_number: Number,
+    card_number: String,
     expiration: String,
-    card_balance: Number
 })
 
-const Card = mongoose.model('Card', CardSchema);
+CardSchema.pre("save", maskCard);
 
-module.exports = {Card, CardSchema};
+function maskCard() {
+
+    const masked = this.card_number.slice(0, 6)
+        .concat('******')
+        .concat(this.card_number
+            .slice(-4));
+
+    this.card_number = masked;
+}
+
+const Card = mongoose.model('Card', CardSchema);
+module.exports = { Card, CardSchema };
