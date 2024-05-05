8aa8d687c12471c57fd9937b331e6fba27bc53f8
Mitko Milev <milevk2@gmail.com>
2024-05-05 14:28:35 +0300

    transaction logic disabled as it is not working with the current database configuration

--- src/server/services/cardService.js
+++ src/server/services/cardService.js
@@ -21,27 +21,23 @@ exports.createCard = async (userId, schemaProvider, cardholder) => {
 //delete credit card
 exports.deleteCard = async (userId, cardId) => {
 
-    const session = await mongoose.startSession();
+   // const session = await mongoose.startSession();
 
-    session.startTransaction();
+   // session.startTransaction();
 
     try {
 
-        await Card.deleteOne({ _id: cardId }).session(session);
-        await User.findByIdAndUpdate({ _id: userId }, { $pull: { cards: {_id: cardId} } }).session(session);
-        await session.commitTransaction();
+        await Card.deleteOne({ _id: cardId });
+        await User.findByIdAndUpdate({ _id: userId }, { $pull: { cards: {_id: cardId} } });
+        // await session.commitTransaction();
         
     }
     catch (err) {
 
         console.log(err.message);
-        await session.abortTransaction();
+        // await session.abortTransaction();
     }
-    finally{
-    
-        session.endSession();
-    }
-}
+   }
 
 
 //list all cards per user
@@ -49,20 +45,15 @@ exports.getUserCards = (userId) => {
 
 
     return Card.find({ owner_id: userId }).lean();
-
-
 }
 
 function generateCardNumber(schemaProvider) {
 
     let cardNumber = `${schemaProvider}`;
-    const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     for (let i = 1; i <= 15; i++) {
 
-        let randIndex = Math.floor(Math.random() * digits.length);
-        const digit = digits[randIndex];
-
+        let digit = Math.floor(Math.random() * 10);
         cardNumber = cardNumber.concat(digit);
     }
     return Number(cardNumber);
