188ec37a836d2d9da076fbb7a8450ba0262f5f75
Mitko Milev <milevk2@gmail.com>
2024-05-07 20:17:21 +0300

    added an error for transferFunds service checking if amount is less or equal to zero

--- src/server/services/transactionService.js
+++ src/server/services/transactionService.js
@@ -7,18 +7,35 @@ const { MongooseError } = require('mongoose');
 //Deposit funds into a user’s account using a credit card.
 exports.depositFunds = async (userId, amount, card_number, card_id) => {
 
+    if (amount <= 0) {
+
+        throw new Error('You can not deposit negative or zero amounts!')
+    }
+
     const deposit_date = new Date(Date.now());
 
     const user = await User.findOne({ _id: userId });
-    user.balance = user.balance + Number(amount);
 
     //TODO: the below must be configured as a DB transaction:
+    user.balance = user.balance + Number(amount);
+
     await user.save();
-    await Deposit.create({ userId, amount, card_number, card_id, deposit_date });
+    return await Deposit.create({ userId, amount, card_number, card_id, deposit_date });
+}
+
+exports.getDeposits = async (userId) => {
+
+    return await Deposit.find({ userId: userId });
+
 }
 
 //Transfer funds to another user account.
 exports.transferFunds = async (sender, receiver, amount) => {
+
+    if (amount <= 0) {
+
+        throw new MongooseError('You can not transfer zero or negative amounts!');
+    }
 
     const tid = generateId('T');
     const transaction_date = new Date(Date.now());
@@ -33,15 +50,14 @@ exports.transferFunds = async (sender, receiver, amount) => {
         throw new MongooseError(`Receiver with customer ID ${receiver} not found!`);
     }
 
-    if(sender == receiver) {
+    if (sender == receiver) {
 
-        throw new Error('You can not transfer funds to yourself!')
+        throw new MongooseError('You can not transfer funds to yourself!')
     }
 
     if (senderDoc.balance < amount) {
 
         throw new MongooseError('You can not transfer amounts larger than your account balance! Please deposit more funds from the dashboard!')
-
     }
 
     senderDoc.balance = Number(senderDoc.balance) - amount;
@@ -49,7 +65,7 @@ exports.transferFunds = async (sender, receiver, amount) => {
 
     await senderDoc.save();
     await receiverDoc.save();
-    await Transaction.create({ tid, sender, receiver, amount, transaction_date });
+    return await Transaction.create({ tid, sender, receiver, amount, transaction_date });
 
 }
 //List all transactions per user.
@@ -65,7 +81,7 @@ exports.getUserTransactions = async (customerId) => {
 function formatDate(transaction) {
 
     const date = transaction.transaction_date;
-    transaction.transaction_date = `${String(date.getDate()).padStart(2,'0')}/${String(date.getMonth()).padStart(2,'0')}/${date.getFullYear()}`
+    transaction.transaction_date = `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth()).padStart(2, '0')}/${date.getFullYear()}`
     return transaction;
 
 }
