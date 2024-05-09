329d50be3ba70318ec5d2b6fd2020cbff662ffa1
Mitko Milev <milevk2@gmail.com>
2024-05-09 11:35:29 +0300

    improved conditional check for route path /deleteCard

--- src/server/middlewares/routeGuardMiddleware.js
+++ src/server/middlewares/routeGuardMiddleware.js
@@ -2,8 +2,10 @@ function routeGuardMiddleware(req, res, next) {
 
     const token = !!req.isToken;
        
-    if (req.path == '/dashboard' || req.path == '/createCard' || req.path == '/deleteCard' || req.path == '/depositFunds' || req.path == '/transfer' || req.path == '/transactions') {
-     
+    
+    if (req.path == '/dashboard' || req.path == '/createCard' || req.path.includes('/deleteCard') || req.path == '/depositFunds' || req.path == '/transfer' || req.path == '/transactions') {
+        
+    
         if (!token) {
             res.status(403);
             const err = {message: 'Not authorized or your session has expired!'};
